cda69a76c4ed22268916dd66eb0c8521f5779b3b
kmruddy <kmruddy@gmail.com>
2020-05-28 18:58:04 -0400

    Adding managed disk

--- DevOpsLab-Intro.tf
+++ DevOpsLab-Intro.tf
@@ -11,3 +11,12 @@ resource "azurerm_resource_group" "rg" {
     name     = "DevOpsLab_Ruddy"
     location = "westus"
 }
+
+resource "azurerm_managed_disk" "catdisk" {
+  name                 = "DevOpsLab_MD"
+  location             = "westus"
+  resource_group_name  = "DevOpsLab_Ruddy"
+  storage_account_type = "Premium_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = "512"
+}
