5810e8e2a750fd8183e72b36c04e7b575a8f85af
kmruddy <kmruddy@gmail.com>
2020-05-28 13:21:47 -0400

    Adding variables reference

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "subscription_id" {
+  description = "The subscription ID of the Azure resources to be used."
+}
+
+variable "client_id" {
+  description = "The client ID of the Azure user to be used."
+}
+
+variable "client_secret" {
+  description = "The client secret or password of the Azure user to be used."
+}
+
+variable "tenant_id" {
+  description = "The tenant ID of the Azure resources to be used."
+}
